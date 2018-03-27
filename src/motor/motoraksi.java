decb16865d325ca099a4dc15e99d40373b4b4af3
rizaja <afrizahelmi35@@gmail.com>
2018-03-27 19:26:14 +0800

    mengisi nilai atribut

--- src/motor/motoraksi.java
+++ src/motor/motoraksi.java
@@ -11,4 +11,37 @@ package motor;
  */
 public class motoraksi {
     public static void main (String[]   args)   {
+        
+        
+        motorclass alfa = new motorclass();
+        System.out.println("motor alfa");
+        alfa.merek="honda";
+        alfa.harga=13000000;
+        alfa.jenis="old";
+        alfa.tipe="S";
+        alfa.warna="merah muda";
+        alfa.cetakInfo();
+        
+        motorclass astrea = new motorclass();
+        System.out.println("motor astrea");
+        astrea.merek="suzuki";
+        astrea.harga=1000000;
+        astrea.jenis="old";
+        astrea.tipe="R";
+        astrea.warna="kuning";
+        astrea.cetakInfo();
+        
+        motorclass cbr = new motorclass();
+        System.out.println("motor cbr");
+        cbr.merek="honda";
+        cbr.harga=20000000;
+        cbr.jenis="new";
+        cbr.tipe="R";
+        cbr.warna="hitam";
+        cbr.cetakInfo();
+        
+                
+                
+                
+    }
 }
